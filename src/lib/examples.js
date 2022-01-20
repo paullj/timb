e8032ac7655de6255cac5458be18c6603491cb29
Paul Lavender-Jones <paul@lavender-jones.com>
2022-01-20 15:07:41 +0000

    📝 Updated examples

--- src/lib/examples.js
+++ src/lib/examples.js
@@ -29,60 +29,64 @@ const examples = [
       "`m` is the current measure (0-7)",
       "and `b`, the beat in the current mesasure (0-7)",
     ],
-    code: "(1+m%2)*110*[1,2,3,4][b%4]",
+    code: "(1 + m % 2) * 110 * [1, 2, 3, 4][b % 4]",
   },
   {
     comments: ["`t` is for time", "and `i` is index (0-63)"],
-    code: "440+t*[0,1,2,1][i%4]",
+    code: "440 + t * [0, 1, 2, 1][i % 4]",
   },
   {
     comments: [
-      "NaN and other non number values are rest notes",
-      "this allows for intesting rhythms",
+      "NaN, undefined and other non number values are rest notes",
+      "this allows for coding intesting rhythms",
     ],
-    code: "440*[2,NaN,1,NaN,1,NaN,1,NaN][i%8]",
+    code: "440 * [2,1,NaN,1][i % 4]",
   },
   {
     comments: [
       "returning an array plays all frequencies as a chord",
       "you can play a maximum of 4 notes at the same time",
     ],
-    code: "[NaN,[440,220,110],[220,110,880]][i%2*(1+m%2)]",
+    code: "[[261.63, 329.628, 391.995], NaN][b % 4]",
   },
   {
     comments: [
       "`f(b,i)` gives you `i` semitones from base frequency `b`",
-      "nb. 440Hz is a4. MAKE THIS A BETTER MSG",
+      "this is useful for making western tonal music (440Hz is a4)",
     ],
-    code: "[0,3,5,7].map(n=>f(440,n))[b%4]",
+    code: "[0, 3, 5, 7].map( n => f(440, n))[b % 4]",
   },
   {
     comments: [
-      "`r(o,t)` gives indexes for euclidean rhythm patterns",
-      "EXPLAIN THIS BETTER goto /about for more",
+      "`r(o,p)` gives euclidean rhythm patterns as arrays of 1s & 0s",
+      "it spreads out `o` onsets across `p` pulses`",
     ],
-    code: "[-50,0][r(3,8)[b]]",
+    code: "[NaN, 0][r(3, 8)[b]]",
   },
   {
     comments: [
-      "`Math` functions are available in scope",
-      "more room for creativity WRITE SOMETHING INTERESTING",
+      "no need for `Math.`, math functions are available in scope",
+      "this gives you more room for creativity",
     ],
-    code: "110*sin(t)+220*cos(2*t)",
+    code: "110 * sin(t) + 220 * cos(2 * t)",
   },
   {
     comments: [
-      "an attempt at twinkle twinkle little star",
+      "here is my attempt at twinkle twinkle little star",
       "can you make it nicer/smaller/better?",
     ],
-    code: "[...'0077997~5544220~7755442~7755442~'].map(n=>f(440,n))[i%32]",
+    code: "[...'0077997~5544220~7755442~7755442'].map(n=>f(440,n))[i%32]",
   },
   {
     comments: [
-      "now its your turn",
-      "remember that you are limited to 64 bytes",
+      "you are limited to 64 bytes, use creative ways to shorten",
+      "eg. `b^0` is equivalent to `b!=0` (every byte counts)!",
     ],
-    code: "'HAVE FUN :)'",
+    code: "b^0 ? 440:-220",
+  },
+  {
+    comments: ["now its your turn", "have fun!"],
+    code: "'*your next masterpiece*'",
   },
 ];
 
