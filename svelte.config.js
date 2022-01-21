6c64cd2ba22001828736f8e279df126cc39b00a2
Paul Lavender-Jones <paul@lavender-jones.com>
2022-01-21 11:08:09 +0000

    🐛 Fixed prerender issue

--- svelte.config.js
+++ svelte.config.js
@@ -4,16 +4,14 @@ const dev = process.env.NODE_ENV === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   kit: {
     adapter: adapter({
       fallback: "index.html",
-      precompress: true,
     }),
     paths: {
       base: dev ? "" : "/timb",
     },
+    router: false,
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
   },
